76cbc751d2e013cb2378e02fe03acc3703042fa5
Pjeferson <paulojeferson34@gmail.com>
2020-02-21 16:05:56 -0300

    Using redux state to control page routing

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
 import createRouter from './createRouter';
 
 export default function Routes() {
-  const signed = false;
+  const signed = useSelector(state => state.auth.signed);
 
   const Router = createRouter(signed);
 
