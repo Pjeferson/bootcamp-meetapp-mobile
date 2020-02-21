3769977befbaa78ba258ce0035c083d24ad64049
Pjeferson <paulojeferson34@gmail.com>
2020-02-21 11:57:29 -0300

    Adding redux access to components

--- App.js
+++ App.js
@@ -1,6 +1,19 @@
 import React from 'react';
+
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+
 import Routes from '~/routes';
+import { store, persistor } from '~/store';
+
+import '~/config/ReactotronConfig';
 
 export default function App() {
-  return <Routes />;
+  return (
+    <Provider store={store}>
+      <PersistGate persistor={persistor}>
+        <Routes />
+      </PersistGate>
+    </Provider>
+  );
 }
