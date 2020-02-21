a137c87ec4dc8bb9232b695c520947ba49e513a6
Pjeferson <paulojeferson34@gmail.com>
2020-02-21 11:18:21 -0300

    Creating a Background component

---
+++ src/components/Background/index.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components/native';
+import { LinearGradient } from 'expo-linear-gradient';
+
+export default styled(LinearGradient).attrs({
+  colors: ['#22202C', '#402845'],
+})`
+  flex: 1;
+`;
