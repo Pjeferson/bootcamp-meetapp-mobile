ce9ccc6616eb9463106db59ccaa1a50a6fc129b5
Pjeferson <paulojeferson34@gmail.com>
2020-02-21 11:38:03 -0300

    Implementing SignIn form

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,8 +1,48 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { View } from 'react-native';
 
-// import { Container } from './styles';
+import logo from '~/assets/logo.png';
+
+import Background from '~/components/Background';
+
+import { Container, Avatar, Form, FormInput, SubmitButton } from './styles';
 
 export default function SignIn() {
-  return <View />;
+  const passwordRef = useRef();
+
+  return (
+    <Background>
+      <Container>
+        <Avatar source={logo} />
+        <Form>
+          <FormInput
+            icon="mail-outline"
+            keyboardType="email-address"
+            autoCorrect={false}
+            autoCapitalize="none"
+            placeholder="Digite seu e-mail"
+            returnKeyType="next"
+            onSubmitEditing={() => {
+              passwordRef.current.focus();
+            }}
+            /* value={email}
+            onChangeText={setEmail} */
+          />
+          <FormInput
+            icon="lock-outline"
+            secureTextEntry
+            placeholder="Sua senha secreta"
+            ref={passwordRef}
+            returnKeyType="send"
+            /* onSubmitEditing={handleSubmit}
+            value={password}
+            onChangeText={setPassword} */
+          />
+          <SubmitButton /* loading={loading} onPress={handleSubmit} */>
+            Entrar
+          </SubmitButton>
+        </Form>
+      </Container>
+    </Background>
+  );
 }
