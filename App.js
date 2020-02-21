002d8e368fa2380a91180300fb311c92fda7c499
Pjeferson <paulojeferson34@gmail.com>
2020-02-21 11:59:46 -0300

    Configuring status bar

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import { StatusBar } from 'react-native';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 
@@ -12,6 +12,7 @@ export default function App() {
   return (
     <Provider store={store}>
       <PersistGate persistor={persistor}>
+        <StatusBar barStyle="light-content" backgroundColor="#22202C" />
         <Routes />
       </PersistGate>
     </Provider>
