ca4e43586a5e87648c4767611c9debee753f5cd7
Pjeferson <paulojeferson34@gmail.com>
2020-02-21 16:03:49 -0300

    Changing api address

--- src/services/api.js
+++ src/services/api.js
@@ -4,7 +4,7 @@ const api = axios.create({
   /* API Development host
     TODO: Use .env
   */
-  baseURL: 'http://192.168.0.22:3333',
+  baseURL: 'http://192.168.0.22:5555',
 });
 
 export default api;
